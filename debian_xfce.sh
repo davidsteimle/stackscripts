801b8511277893b2b13b357569e6836cced54fbf
David Steimle <davidsteimle@gmail.com>
2021-02-04 15:57:53 -0500

    Create debian_xfce.sh

---
+++ debian_xfce.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Create a log file
+LOGFILE="/root/stackscript.log"
+
+# Update the server
+apt update >> $LOGFILE 2>&1
+apt upgrade -y >> $LOGFILE 2>&1
+apt dist-upgrade -y >> $LOGFILE 2>&1
+apt autoremove -y >> $LOGFILE 2>&1
+apt autoclean -y >> $LOGFILE 2>&1
+
+# Install desktop environment
+apt install task-xfce-desktop -y >> $LOGFILE 2>&1
+
+# Install a browser -- add any other desired software
+apt install chromium -y >> $LOGFILE 2>&1
+
+# Install xrdp
+apt install xrdp -y >> $LOGFILE 2>&1
+adduser xrdp ssl-cert >> $LOGFILE 2>&1
+systemctl restart xrdp >> $LOGFILE 2>&1
+
+# Open firewall -- the bad way
+ufw allow 3389 >> $LOGFILE 2>&1
+
+# Log completion
+echo "*** stack script complete ***" >> $LOGFILE
